1b3a304eb516f2e4a4a3582e37ab16fab00515c3
VIPon4ik <umniyd@gmail.com>
2023-09-09 12:13:39 +0300

    changing icons

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+});
