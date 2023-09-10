ead31e32fd9d0a05f3ea9486eb0469d48325fbee
Mahavr <mahavr2122003@gmail.com>
2023-09-10 23:53:07 +0300

    fix and add adaptive

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -7,7 +7,7 @@
       const isMenuOpen =
          openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
+      mobileMenu.classList.toggle('is-hidden');
 
       const scrollLockMethod = !isMenuOpen
          ? 'disableBodyScroll'
@@ -21,7 +21,7 @@
    // Close the mobile menu on wider screens if the device orientation changes
    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
       if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
+      mobileMenu.classList.remove('is-hidden');
       openMenuBtn.setAttribute('aria-expanded', false);
       bodyScrollLock.enableBodyScroll(document.body);
    });
